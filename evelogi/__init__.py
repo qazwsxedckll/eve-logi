200a5e2102d37a634d9e2d19bdf5cf157f3e8f6a
Evilolipop <decai.wang@foxmail.com>
2021-04-21 21:26:38 +0800

    fix bug

--- evelogi/__init__.py
+++ evelogi/__init__.py
@@ -48,7 +48,7 @@ def register_logger(app):
     # log to file
     if not app.debug:
         file_handler = RotatingFileHandler('logs/{}.log'.format(time.strftime(
-            '%Y-%m-%d %H:%M:%S', time.localtime())), maxBytes=10*1024*1024, backupCount=10)
+            '%Y-%m-%d_%H:%M:%S', time.localtime())), maxBytes=10*1024*1024, backupCount=10)
         file_handler.setFormatter(formatter)
         file_handler.setLevel(logging.INFO)
 
