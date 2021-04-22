9faabc3b773b36fb4f664bbbd65e07b71d2990d1
Evilolipop <decai.wang@foxmail.com>
2021-04-22 20:55:25 +0800

    compress css

--- evelogi/tailwind/tailwind.config.js
+++ evelogi/tailwind/tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    purge: [],
+    purge: [
+        '../templates/*.html',
+        '../templates/**/*.html',
+    ],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
